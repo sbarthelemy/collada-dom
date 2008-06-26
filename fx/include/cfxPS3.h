4c241af3f49fc163dd04b938c8cc3a9e8d5634be
sceahklaw <sceahklaw@dfea1ed5-6b0d-0410-973e-e49e28f48b26>
2008-06-26 20:08:43 +0000

    Add CfxPS3.h to sourceforge
    git-svn-id: https://collada-dom.svn.sourceforge.net/svnroot/collada-dom/trunk@572 dfea1ed5-6b0d-0410-973e-e49e28f48b26

---
+++ fx/include/cfxPS3.h
@@ -0,0 +1,16 @@
+/* --- DO NOT REMOVE THIS LINE --- SNIP_SDK_COPYRIGHT_C_BEGIN */
+/* --- DO NOT REMOVE THIS LINE --- SNIP_SDK_COPYRIGHT_C_END */
+
+#ifndef _CFX_PS3_H_
+#define _CFX_PS3_H_
+
+#include <PSGL/psgl.h>
+
+#define CG_FILE_TYPE CG_BINARY
+
+inline void cfxPrint(const char* str, ...)
+{
+	printf(str);
+}
+
+#endif
