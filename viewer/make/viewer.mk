010993111ef7ebda7fd8d1bed4f08b2e4f1f7987
sceahklaw <sceahklaw@dfea1ed5-6b0d-0410-973e-e49e28f48b26>
2008-09-09 01:26:35 +0000

    update all the else and endif to be compatible with make 3.81
    git-svn-id: https://collada-dom.svn.sourceforge.net/svnroot/collada-dom/trunk@660 dfea1ed5-6b0d-0410-973e-e49e28f48b26

--- viewer/make/viewer.mk
+++ viewer/make/viewer.mk
@@ -5,8 +5,10 @@ src += projects/vc8-PS3/main.cpp projects/vc8-PS3/BrowserUI.cpp
 
 ifneq ($(findstring $(os),linux mac),)
 ccFlags += -fPIC
-else ifeq ($(os),windows)
+else 
+ifeq ($(os),windows)
 ccFlags += -DDOM_DYNAMIC -DDOM_EXPORT
+endif
 endif
 
 ifneq ($(findstring libxml,$(xmlparsers)),)
@@ -47,13 +49,15 @@ ifeq ($(os),linux)
 targets += $(addprefix $(outPath),$(libName).a)
 targets += $(addprefix $(outPath),$(libName).so)
 
-else ifeq ($(os),windows)
+else 
+ifeq ($(os),windows)
 # On Windows we build a static lib and a DLL
 windowsLibName := libcollada$(colladaVersionNoDots)dom
 targets += $(addprefix $(outPath),$(windowsLibName)$(debugSuffix).a)
 targets += $(addprefix $(outPath),$(windowsLibName)$(libVersionNoDots)$(debugSuffix).dll)
 
-else ifeq ($(os),mac)
+else 
+ifeq ($(os),mac)
 # On Mac we build a framework
 targets += $(addprefix $(outPath),Collada$(colladaVersionNoDots)Dom$(debugSuffix).framework)
 frameworkHeadersPath = $(framework)/Versions/$(libVersion)/Headers
@@ -68,7 +72,8 @@ copyFrameworkResourcesCommand = cp -R make/macFrameworkResources/* $(frameworkRe
   sed $(sedReplaceExpression) make/macFrameworkResources/Info.plist > $(frameworkResourcesPath)/Info.plist && \
   sed $(sedReplaceExpression) make/macFrameworkResources/English.lproj/InfoPlist.strings > $(frameworkResourcesPath)/English.lproj/InfoPlist.strings
 
-else ifeq ($(os),ps3)
+else 
+ifeq ($(os),ps3)
 # On PS3 we build a static lib, since PS3 doesn't support shared libs
 
 
@@ -120,6 +125,9 @@ libOpts += -lm -lio_stub -lcgc -lgcm_cmd -lgcm_sys_stub -lsysmodule_stub -lresc_
 
 targets := $(outPath)viewer$(exeSuffix)
 
+endif
+endif
+endif
 endif
 
 ifeq ($(os),ps3)
